624d2f647481fdb4b09e749c9c4a03c65175affc
Mark Hario <hario.mark@gmail.com>
2017-12-20 19:31:19 -0700

    Building ES6 for mocha

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,15 @@
-const path = require('path')
-
 module.exports = {
 	entry: './app/main.js',
 	output: {
 		filename: 'public/bundle.js'
 	},
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env']
-        }
-      }
-    ]
-  }
+	devtool: 'source-map',
+	module: {
+		loaders: [
+			{
+				test: /jsx?$/,
+				exclude: /(node_modules|bower_components)/
+			}
+		]
+	}
 }
