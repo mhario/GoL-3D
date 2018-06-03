688f323e0492959b2d082cba57e1c18d47f4acfb
Mark Hario <hario.mark@gmail.com>
2018-06-03 16:15:45 -0600

    webpack fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,26 @@
+const path = require('path')
+/* global __dirname */
+
 module.exports = {
 	entry: './app/main.js',
 	output: {
 		filename: 'public/bundle.js'
 	},
-	devtool: 'source-map',
+	// devtool: 'source-map',
 	watch: true,
 	module: {
 		rules: [
-			{ test: /jsx?$/,
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						compact: false
-					}
-				}]
-			},
+		// 	{ test: /jsx?$/,
+		// 		use: [{
+		// 			loader: 'babel-loader',
+		// 			options: {
+		// 				compact: false
+		// 			}
+		// 		}]
+		// 	},
 			{
 				test: /\.js$/,
+				include: path.resolve(__dirname, 'app'),
 				exclude: /node_modules/,
 				use: [
 					'babel-loader',
