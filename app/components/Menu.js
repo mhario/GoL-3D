76ff627f79c40c3b59b15d796ecc0e3fe76663b8
Mark Hario <hario.mark@gmail.com>
2018-06-20 11:33:14 -0600

    progress save

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -5,19 +5,32 @@ import TabBar from './Tabs/'
 export class Menu extends React.Component {
 	constructor() {
 		super()
+		this.state = {
+			playing: false,
+			firstGame: new Game('game')
+		}
+
+		this.state.firstGame.buildBoard()
+
+		this.play = this._play.bind(this)
+	}
+
+	_play() {
+		this.setState({playing: true})
 	}
 
 	render() {
-		let firstGame = new Game('game')
-		firstGame.buildBoard()
-
 		return (
 			<div className="menu">
 				<h1>Game Of Life</h1>
 				<h3>Mark Hario</h3>
-
+				{
+					this.state.playing
+						? <button>Playing!</button>
+						: <button onClick={this.play}>Play</button>
+				}
 				<TabBar
-					game={firstGame}
+					game={this.state.firstGame}
 				/>
 			</div>
 		)
